b3978f134e6a4ff2cf56934b078492747c66bdaf
Byungsoo Kim <contact.byungsoo@gmail.com>
2017-03-03 15:55:53 +0900

    update scripts before testing edge weight

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
-# 24-02-17 Fri.
-python geonet_run.py --result_dir=result/face_whole_0.01_128 --pretrained_model_checkpoint_path=log/face_whole_0.01_128/geonet.ckpt --data_dir=data/10FacialModels_whole --noise_level=0.01
-python geonet_run.py --result_dir=result/face_whole_0.01_64  --pretrained_model_checkpoint_path=log/face_whole_0.01_64/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.01
-python geonet_run.py --result_dir=result/face_whole_0.01_32  --pretrained_model_checkpoint_path=log/face_whole_0.01_32/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.01
+# python geonet_run.py --result_dir=result/face_whole_0.01_512 --pretrained_model_checkpoint_path=log/face_whole_0.01_512/geonet.ckpt-100000 --data_dir=data/10FacialModels_whole --noise_level=0.01
+# python geonet_run.py --result_dir=result/face_whole_0.01_256 --pretrained_model_checkpoint_path=log/face_whole_0.01_256/geonet.ckpt-100000 --data_dir=data/10FacialModels_whole --noise_level=0.01
 
-python geonet_run.py --result_dir=result/face_whole_0.005_128 --pretrained_model_checkpoint_path=log/face_whole_0.005_128/geonet.ckpt-100000 --data_dir=data/10FacialModels_whole --noise_level=0.005
-python geonet_run.py --result_dir=result/face_whole_0.005_64  --pretrained_model_checkpoint_path=log/face_whole_0.005_64/geonet.ckpt  --data_dir=data/10FacialModels_whole --noise_level=0.005
-python geonet_run.py --result_dir=result/face_whole_0.005_32  --pretrained_model_checkpoint_path=log/face_whole_0.005_32/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.005
+python geonet_run.py --result_dir=result/face_whole_0.005_512 --pretrained_model_checkpoint_path=log/face_whole_0.005_512_cont/geonet.ckpt-95000 --data_dir=data/10FacialModels_whole --noise_level=0.005
+# python geonet_run.py --result_dir=result/face_whole_0.005_256 --pretrained_model_checkpoint_path=log/face_whole_0.005_256/geonet.ckpt-100000 --data_dir=data/10FacialModels_whole --noise_level=0.005
+
+# 24-02-17 Fri.
+# python geonet_run.py --result_dir=result/face_whole_0.01_128 --pretrained_model_checkpoint_path=log/face_whole_0.01_128/geonet.ckpt --data_dir=data/10FacialModels_whole --noise_level=0.01
+# python geonet_run.py --result_dir=result/face_whole_0.01_64  --pretrained_model_checkpoint_path=log/face_whole_0.01_64/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.01
+# python geonet_run.py --result_dir=result/face_whole_0.01_32  --pretrained_model_checkpoint_path=log/face_whole_0.01_32/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.01
+
+# python geonet_run.py --result_dir=result/face_whole_0.005_128 --pretrained_model_checkpoint_path=log/face_whole_0.005_128/geonet.ckpt-100000 --data_dir=data/10FacialModels_whole --noise_level=0.005
+# python geonet_run.py --result_dir=result/face_whole_0.005_64  --pretrained_model_checkpoint_path=log/face_whole_0.005_64/geonet.ckpt  --data_dir=data/10FacialModels_whole --noise_level=0.005
+# python geonet_run.py --result_dir=result/face_whole_0.005_32  --pretrained_model_checkpoint_path=log/face_whole_0.005_32/geonet.ckpt-100000  --data_dir=data/10FacialModels_whole --noise_level=0.005
--- run_train.sh
+++ run_train.sh
@@ -1,5 +1,11 @@
+python geonet_train.py --log_dir=log/face_whole_0.005_512_cont  --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=1 --image_width=512 --image_height=512 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.005 --weight_on=False --pretrained_model_checkpoint_path=log/face_whole_0.005_512/geonet.ckpt-90000
+
+# 27-02-17 Mon., train on noise 0.010, 0.005 patch size 256 with batch 4, 10 faces, whole gaussian noise
+# python geonet_train.py --log_dir=log/face_whole_0.01_256  --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=4 --image_width=256 --image_height=256 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.010 --weight_on=False
+# python geonet_train.py --log_dir=log/face_whole_0.005_256 --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=4 --image_width=256 --image_height=256 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.005 --weight_on=False
+
 # 26-02-17 Sun., re-train on noise 0.005 patch size 128 with batch 16, 10 faces, whole gaussian noise
-python geonet_train.py --log_dir=log/face_whole_0.005_128_2 --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=16 --image_width=128 --image_height=128 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.005 --weight_on=False --pretrained_model_checkpoint_path=log/face_whole_0.005_128/geonet.ckpt
+# python geonet_train.py --log_dir=log/face_whole_0.005_128_2 --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=16 --image_width=128 --image_height=128 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.005 --weight_on=False --pretrained_model_checkpoint_path=log/face_whole_0.005_128/geonet.ckpt
 
 # # 26-02-17 Sun., re-train on noise 0.010 patch size 128 with batch 16, 10 faces, whole gaussian noise
 # python geonet_train.py --log_dir=log/face_whole_0.01_128_2 --data_dir=data/10FacialModels_whole --file_list=train_mat.txt --batch_size=16 --image_width=128 --image_height=128 --max_steps=100000 --initial_learning_rate=0.01 --decay_steps=40000 --transform=True --noise_level=0.010 --weight_on=False
